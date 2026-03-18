745ac1dfc0b481f5e74a789c3c717ccb1e1058b4
mihaelaalinabucea-maker <mihaelaalina.bucea@ulbsibiu.ro>
2026-03-18 16:11:47 +0200

    laborator2

---
+++ main.py
@@ -0,0 +1,55 @@
+print(len('buna ziua'))
+print('buna ziua' .strip('b'))
+print('buna ziua' .split())
+print('buna ziua' .replace('ziua', 'buna'))
+print(len('foc de tabara'))
+print('foc de tabara' .startswith('tabara'))
+print('foc de tabara' .endswith('tabara'))
+print('foc de tabara' .upper())
+print(len('MaSina'))
+print('MaSina' .lower())
+name1='ziua'
+name2='studenti'
+print(f'buna {name1} domnilor {name2}')
+print(f'buna {name1} domnilor {name2}')
+
+
+
+#boolean
+bool(True)
+bool(False)
+print(5==5)
+print(5==6)
+
+
+
+#LIST
+MY_LYST=[1,2,'5',True]
+len(MY_LYST)
+print(MY_LYST.index('5'))
+print(MY_LYST.count(2))
+print(MY_LYST[3])
+print(MY_LYST[1:])
+print(MY_LYST[:1])
+print(MY_LYST[-1])
+print(MY_LYST[::-1])
+print(MY_LYST[0:3:2])
+print(MY_LYST*2)
+print(MY_LYST+[100])
+print(MY_LYST.extend([100,150]))
+print(MY_LYST.insert(2,'???'))
+print([1,2,5].pop())
+print([1,2,5].pop(1))
+print([1,2,5].remove(2))
+print([1,2,5,3].sort())
+print([1,2,5,3].reverse())
+print(min([1,2,3,4,5]))
+print(max([1,2,3,4,5]))
+print(sum([1,2,3,4,5]))
+
+
+
+
+
+
+
